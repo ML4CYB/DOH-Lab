4d6a14b8bbde574fa0971d75e22ca4974d4f90f1
Tjudd04 <140094202+Tjudd04@users.noreply.github.com>
2024-01-20 12:14:53 -0500

    Delete doHlyzer-Testing-Program-main/doHlyzer-Testing-Program-main/Dohlyzer_Program directory

--- doHlyzer-Testing-Program-main/doHlyzer-Testing-Program-main/Dohlyzer_Program/inc/timezone.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-export TZ=US/Michigan
--- doHlyzer-Testing-Program-main/doHlyzer-Testing-Program-main/Dohlyzer_Program/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker run -it dohtest /bin/bash
-container_id=$(docker ps -l --format "{{.ID}}")
-cd CSV_files
-docker start $container_id
-
-# Replace capturedPackets.csv with the name of the csv file you want to copy
-docker cp $container_id:/code/test/capture.csv .
-docker stop $container_id
